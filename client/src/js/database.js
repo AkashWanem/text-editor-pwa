efd4accf7cb443480d4f4ac7e4c5c43770c0ff1a
Akash Limbu <akashwanem@hotmail.com>
2022-05-13 14:26:20 +0100

    added a method that updates database through put request

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,19 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  console.error('request to update db via put')
+  const jateDB = await openDB("jate", 1);
+  const tx = jateDB.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: id, value: value });
+  const result = await request;
+  console.log("Data is saved to the database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented')
+};
 
 initdb();
